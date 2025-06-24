1c5b055ba2ef203baf8a62a99808fc9f2c9fb37b
ciara <ciaraugasmoy@gmail.com>
2025-06-24 18:09:31 -0400

    samples test

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("hello world");
+      String txt = "lolocate where '' occurs!";
+      System.out.println(txt.indexOf("locate"));
+    }
+  }
